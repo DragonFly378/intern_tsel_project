3c010e41833f28ac36a659c47817efd1e3ae2a58
Asa yuaziva <yuazivaasa@gmail.com>
2025-08-05 20:19:43 +0700

    update data js

---
+++ data.js
@@ -0,0 +1,78 @@
+const regionalData = {
+  SUMBAGUT: {
+    revTotal: {
+      target: "-",
+      achv: "-",
+      mtd: 9.45,
+      mom: "1.29%",
+      ytd: "-10.00%",
+      yoy: "-2.58%",
+      rank: 7
+    },
+    revExisting: {
+      target: "-",
+      achv: "-",
+      mtd: 9.45,
+      mom: "1.29%",
+      ytd: "-10.00%",
+      yoy: "-2.58%"
+    }
+  },
+  SUMBAGTENG: {
+    revTotal: {
+      target: "-",
+      achv: "-",
+      mtd: 8.12,
+      mom: "2.15%",
+      ytd: "-5.00%",
+      yoy: "-1.20%",
+      rank: 5
+    },
+    revExisting: {
+      target: "-",
+      achv: "-",
+      mtd: 8.12,
+      mom: "2.15%",
+      ytd: "-5.00%",
+      yoy: "-1.20%"
+    }
+  },
+  SUMBAGSEL: {
+    revTotal: {
+      target: "-",
+      achv: "-",
+      mtd: 6.55,
+      mom: "0.95%",
+      ytd: "-8.00%",
+      yoy: "-3.10%",
+      rank: 8
+    },
+    revExisting: {
+      target: "-",
+      achv: "-",
+      mtd: 6.55,
+      mom: "0.95%",
+      ytd: "-8.00%",
+      yoy: "-3.10%"
+    }
+  },
+  "WESTERN JABOTABEK": {
+    revTotal: {
+      target: "-",
+      achv: "-",
+      mtd: 9.45,
+      mom: "1.29%",
+      ytd: "-10.00%",
+      yoy: "-2.58%",
+      rank: 7
+    },
+    revExisting: {
+      target: "-",
+      achv: "-",
+      mtd: 9.45,
+      mom: "1.29%",
+      ytd: "-10.00%",
+      yoy: "-2.58%"
+    }
+  }
+};
